0333219877a1a3875d5214bf65b7e2e737f1e262
Gaurav Bisht <gauravbisht80971@gmail.com>
2026-07-27 21:53:27 +0530

    get post put del routes

--- backend/api.py
+++ backend/api.py
@@ -2,7 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get('/hello')
 
 def helloworld():
     return {'message' :'hello world!'}
+
+@app.post('/items')
+def create_items(item: str):
+    items.append(item)
+    return items
+
+@app.get('/items/{item_id}')
+def get_item(item_id: int):
+    item = items[item_id]
+    return item
+
+@app.put('/items/{id}')
+def update(id: int, item:str):
+    return {"item": id, "item":item}
