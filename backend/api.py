492e96dd38db991eec10a04b391762cab14d4f1c
Gaurav Bisht <gauravbisht80971@gmail.com>
2026-07-28 21:39:28 +0530

    parameters

--- backend/api.py
+++ backend/api.py
@@ -1,24 +1,10 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app= FastAPI()
 
-items = []
-
-@app.get('/hello')
-
-def helloworld():
-    return {'message' :'hello world!'}
-
-@app.post('/items')
-def create_items(item: str):
-    items.append(item)
-    return items
-
-@app.get('/items/{item_id}')
-def get_item(item_id: int):
-    item = items[item_id]
-    return item
-
-@app.put('/items/{id}')
-def update(id: int, item:str):
-    return {"item": id, "item":item}
+@app.get('/user/{id}/info')
+def info(id:int, email:bool=False):
+    if email:
+        return{'id':id,'email':"u not cooked"}
+    else:
+        return{'id':id,'email':"nope u cooked"}
