fd39c86c8fc3e8f7fa77c5c881d93070f60306e8
Gaurav Bisht <gauravbisht80971@gmail.com>
2026-08-01 15:01:55 +0530

    file and save files

--- backend/api.py
+++ backend/api.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI
-from pydantic import BaseModel,EmailStr,conint
-
-
+from fastapi import FastAPI, Form, UploadFile, File
 
 app = FastAPI()
 
-class User(BaseModel):
-    name:str
-    mail:EmailStr
-    age:conint(ge=10)
+@app.post("/form/")
+async def FORM_DATA(name:str=Form(...),age:int=Form(...)):
+    return {"name": name,"message": "login OK"}
+
+@app.post("/file/")
+async def file(file: UploadFile = File(...)):
+    return {"filename": file.filename}
 
 
-
-@app.post("/register/")
-async def reg_user(user:User):
-    return user
+@app.post("/savefile/")
+async def save(file: UploadFile = File(...)):
+    with open(f'uploads/{file.filename}',"wb") as f:
+        f.write(file.file.read())
+    return {"message":"FILE UPLOADED"}
