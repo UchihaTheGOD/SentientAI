8778d2814fc9b92ed6dccdc0a47f4e49bb4d661c
Gaurav Bisht <gauravbisht80971@gmail.com>
2026-08-02 22:09:05 +0530

    LIST UPLOAD

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI, Form, UploadFile, File
-
+from typing import List
 app = FastAPI()
 
-@app.post("/form/")
-async def FORM_DATA(name:str=Form(...),age:int=Form(...)):
-    return {"name": name,"message": "login OK"}
+# @app.post("/form/")
+# async def FORM_DATA(name:str=Form(...),age:int=Form(...)):
+#     return {"name": name,"message": "login OK"}
 
-@app.post("/file/")
-async def file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+# @app.post("/file/")
+# async def file(file: UploadFile = File(...)):
+#     return {"filename": file.filename}
 
 
-@app.post("/savefile/")
-async def save(file: UploadFile = File(...)):
-    with open(f'uploads/{file.filename}',"wb") as f:
-        f.write(file.file.read())
-    return {"message":"FILE UPLOADED"}
+# @app.post("/savefile/")
+# async def save(file: UploadFile = File(...)):
+#     with open(f'uploads/{file.filename}',"wb") as f:
+#         f.write(file.file.read())
+#     return {"message":"FILE UPLOADED"}
+
+
+@app.post("/multifile/")
+async def multi_file(files: List[UploadFile]=File(...)):
+    return {"filename": [file.filename for file in files]}
