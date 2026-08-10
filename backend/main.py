61bcdb375c45fae9e2520d5ec79fb158a002fc8b
Gaurav Bisht <gauravbisht80971@gmail.com>
2026-08-11 00:30:32 +0530

    JWT WORKING and Basic LOGIN

--- backend/main.py
+++ backend/main.py
@@ -86,11 +86,35 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp":expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
-
+#TOKEN
 @app.post("/token")
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise HTTPException{
-            
-        }
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="INCORRECT USERNAME AND PASSWORD",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return{"access_token":access_token,"token_type":"bearer"}
+def verify_token(token: str = Depends(oauth_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=403,detail="Token is invalid or expired")
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=403, detail="Token is invalid or expired")
+
+#Used to check if token is valid or not
+@app.get("/verify-token/{token}")
+async def verify_user_token(token: str):
+    verify_token(token=token)
+    return {"message": "Token is valid"}
+
+
